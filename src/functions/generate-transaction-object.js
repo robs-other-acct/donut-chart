b950c32d0b77a4642a2d89095839190aa465e622
robs-other-acct <robmyroy@gmail.com>
2019-09-08 15:05:56 -0700

    Allow variable rounding

--- src/functions/generate-transaction-object.js
+++ src/functions/generate-transaction-object.js
@@ -1,9 +1,11 @@
-const generateTransactionObject = function(state, idealState, categories){
+const generateTransactionObjectFromRoundedData = function(state, idealState, categories){
         let result = [];
         let numberOfResolvedCategories = 0;
         let desiredChangeByCat = {};
         let desiredChangeByAmt = {};
+        step=0;
         while (numberOfResolvedCategories < categories.length){
+            step++;
            let  anyEffectOfFirstPass = false;
             for(let i=0; i<categories.length; i++){
                 let cat = categories[i];
@@ -38,8 +40,9 @@ const generateTransactionObject = function(state, idealState, categories){
                 let desiredChange = desiredChangeByCat[cat];
                 
                 if(desiredChange >=0) continue;
-                numberOfResolvedCategories ++;
-                let positiveDesiredChange = Object.keys(desiredChangeByAmt).find(change => change >0);
+                let positiveDesiredChange = Object.keys(desiredChangeByAmt).find(change => change >0
+                    && desiredChangeByAmt[change].length);
+                if (positiveDesiredChange === undefined){return result;}
                 let antiCat = desiredChangeByAmt[positiveDesiredChange].pop();
                 let transferAmt = Math.min(desiredChangeByCat[antiCat],
                     -desiredChange);
@@ -53,17 +56,55 @@ const generateTransactionObject = function(state, idealState, categories){
                 newSiblingCats.concat(antiCat) 
                     : 
                 [antiCat];
-                
+                numberOfResolvedCategories ++;
+                break;
             }
         }
-        console.log(numberOfResolvedCategories);
-
     }
     return result;
 
 }
 
-// export default generateTransactionObject;
+
+generateTransactionObject = function(state, idealStateInPercentage, categories, roundingPercentage){
+    let roundedState = {};
+    let roundedIdealState = {};
+    let total = 0;
+    categories.forEach(cat => {total += state[cat] || 0});
+    total = parseInt(total);
+    categories.forEach(cat => 
+        {roundedState[cat]= Math.ceil(state[cat] * 100/(total*roundingPercentage))
+        roundedIdealState[cat] = Math.floor(idealStateInPercentage[cat] /roundingPercentage)});
+
+        let transactionsInPercentages = generateTransactionObjectFromRoundedData(
+            roundedState, 
+            roundedIdealState,
+            categories);
+            console.log(transactionsInPercentages);
+        let transactionsBySource = {};
+        let transactionsInDollars = transactionsInPercentages.map(
+            t => {
+                let newTransaction = [t[0], t[1], t[2] * roundingPercentage*total/100];
+                transactionsBySource[t[0]] = transactionsBySource[t[0]] || [];
+                transactionsBySource[t[0]].push(newTransaction);
+                return newTransaction;}
+        );
+        console.log(transactionsInDollars);
+        Object.keys(transactionsBySource).forEach(
+            sourceCat => {
+                if(idealStateInPercentage[sourceCat] === 0){
+                let sum = 0;
+                transactionsBySource[sourceCat].forEach(
+                    transaction => {sum += transaction[2];}
+                )
+                let remainder = state[sourceCat] - sum;
+                transactionsBySource[sourceCat][0][2] += remainder;
+            }
+        }
+        )    
+        return transactionsInDollars;
+}
 
 const categories = 'abcde'.split('');
-console.log(generateTransactionObject({a: 2, b: 3, c: 3, e: 2}, {b: 4, c: 5, d: 1}, categories));
+console.log(generateTransactionObject(
+    {a: 75, b: 100, c: 291, d: 0, e: 25}, {a:0, b: 40, c: 20, d: 40, e:0}, categories, .1));
